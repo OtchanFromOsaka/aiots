ddb5457c2c3c73f364712e449e32b95c5034d42b
OtchanFromOsaka <147917062+OtchanFromOsaka@users.noreply.github.com>
2025-02-08 21:42:28 +0900

    Add data fetching functionality and remove unused test page

--- projects/nuxt-sample/pages/index.vue
+++ projects/nuxt-sample/pages/index.vue
@@ -4,10 +4,24 @@ import { ref } from "vue";
 import { MyButton } from "../../../packages/aio-ui-vue";
 
 const count = ref(0);
+
+const response = ref(null);
+
+async function fetchData() {
+	const res = await fetch("/api/v1/test");
+	const data = await res.json();
+	response.value = data;
+};
 </script>
 
 <template>
 	<h1>Index</h1>
-	<p>count: {{ count }}</p>
-	<my-button @click="count++">increment</my-button>
+	<p style="margin-top: 50px; margin-bottom: 5px;">count: {{ count }}</p>
+	<div>
+		<my-button @click="count++">increment</my-button>
+	</div>
+	<p style="margin-top: 50px; margin-bottom: 5px;">response data: {{ response }}</p>
+	<div>
+		<my-button @click="fetchData">/api/v1/test</my-button>
+	</div>
 </template>
