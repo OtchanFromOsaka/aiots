abb5a86585fe57ad7319bdea1f660930858d9c08
OtchanFromOsaka <147917062+OtchanFromOsaka@users.noreply.github.com>
2025-03-16 16:33:14 +0900

    Add AioSideNav component with customizable features and update documentation

--- packages/aio-hono-jsx/app/routes/index.tsx
+++ packages/aio-hono-jsx/app/routes/index.tsx
@@ -1,6 +1,7 @@
 import { createRoute } from "honox/factory";
 
 import { AioAppBar } from "@/components/aio-app-bar";
+import { AioSideNav } from "@/components/aio-side-nav";
 import { Counter } from "@/islands/counter";
 import { ThemeToggleSwitch } from "@/islands/theme-toggle-switch";
 
@@ -10,10 +11,132 @@ export default createRoute(async (c) => {
 			<AioAppBar title="AIO-UI">
 				<ThemeToggleSwitch />
 			</AioAppBar>
-			<main style={{ marginTop: "80px", padding: "16px" }}>
+			<AioSideNav>
+				<h2>Navigation</h2>
+				<ul style={{ listStyle: "none", padding: "0", marginTop: "16px" }}>
+					<li style={{ marginBottom: "8px" }}>
+						<a
+							href="/"
+							style={{ textDecoration: "none", color: "var(--text-color)" }}
+						>
+							Home
+						</a>
+					</li>
+					<li style={{ marginBottom: "8px" }}>
+						<a
+							href="/about"
+							style={{ textDecoration: "none", color: "var(--text-color)" }}
+						>
+							About
+						</a>
+					</li>
+					<li style={{ marginBottom: "8px" }}>
+						<a
+							href="/contact"
+							style={{ textDecoration: "none", color: "var(--text-color)" }}
+						>
+							Contact
+						</a>
+					</li>
+				</ul>
+			</AioSideNav>
+			<main style={{ marginTop: "80px", marginLeft: "266px", padding: "16px" }}>
 				<div>
 					<Counter />
 				</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
 			</main>
 		</>
 	);
